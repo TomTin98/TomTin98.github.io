4a6cf117049fad474bfe9ab3b71349c21d05ea93
TomTin98 <tommy.tindeland98@gmail.com>
2016-05-27 20:00:27 +0200

    Added everything i need for NEW website

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " Test rettigheter ting</p>";
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Hjem</a></li>" +
+"<li><a href='forskjelligt.html'>Forskjelligt</a></li>" +
+"<li><a href='omvårbedrift.html'>Om vår bedrift</a></li>" + "<li><a href='testside.html'>Testside</a></li>"
+"</ul>";
