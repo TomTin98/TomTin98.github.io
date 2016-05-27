946d136df02fd700ba6d1be61b1974537aa8e8e4
TomTin98 <tommy.tindeland98@gmail.com>
2016-05-27 19:50:03 +0200

    deleted old stuff

--- javascripts/main.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("foot01").innerHTML =
-"<p>&copy;  " + new Date().getFullYear() + " Tommy T rettigheter</p>";
-document.getElementById("nav01").innerHTML =
-"<ul id='menu'>" +
-"<li><a href='index.html'>Hjem</a></li>" +
-"<li><a href='forskjelligt.html'>Forskjelligt</a></li>" +
-"<li><a href='omvårbedrift.html'>Om vår bedrift</a></li>" + "<li><a href='testside.html'>Testside</a></li>"
-"</ul>";
