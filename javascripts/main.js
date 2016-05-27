d505a6f748f68506d9ace8fbf3cd2594e402cfc2
TomTin98 <tommy.tindeland98@gmail.com>
2016-05-27 19:47:41 +0200

    Added Javascript to website
    before, there was nothing, now theres something

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
-console.log('This would be the main JS file.');
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " Tommy T rettigheter</p>";
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Hjem</a></li>" +
+"<li><a href='forskjelligt.html'>Forskjelligt</a></li>" +
+"<li><a href='omvårbedrift.html'>Om vår bedrift</a></li>" + "<li><a href='testside.html'>Testside</a></li>"
+"</ul>";
